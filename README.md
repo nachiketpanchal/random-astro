c5962abe431b3d5ef6546587669f4ede222d7219
nachiketpanchal <nnpanchal007@gmail.com>
2022-01-19 00:43:58 -0400

    Implemented Like post feature

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Welcome to Random Astro
 
 Random Astro is an application to have fun with random astronomical images and videos.
+
+## Working
+
+This application uses NASA's Astronomy Picture of the Day (APOD) microservice to get random 10 astronomical images. The user can like this posts which are stored in the browser's localstorage.
+
+## Libraries Used
+Axios - For API requests
+Bootstrap - For responsive structure and design
+dotenv - For environment variables
+Font Awesome - For web icons
+md5 - For unique id generation of posts
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import md5 from "md5";
 
 function Home() {
     const [data, setData] = useState([]);
+    const [likedImages, setLikedImages] = useState({});
+    const like = (event) => {
+        let likes = JSON.parse(localStorage.getItem("likes"));
+        const imageId = event.currentTarget.id;
+        if(likes[imageId] === undefined){
+            likes[imageId] = 1;
+            event.currentTarget.innerHTML = '<i class="fas fa-heart"></i>';
+        }else{
+            delete likes[imageId];
+            event.currentTarget.innerHTML = '<i class="far fa-heart"></i>';
+        }
+        setLikedImages(likes);
+        localStorage.setItem("likes", JSON.stringify(likes));
+    };
     useEffect(()=>{
+        localStorage.getItem("likes") === null 
+            ? localStorage.setItem("likes", JSON.stringify({}))
+            : console.log('Liked data available');
+        setLikedImages(JSON.parse(localStorage.getItem("likes")));
+
         axios.get('https://api.nasa.gov/planetary/apod?count=10&api_key=' + process.env.REACT_APP_NASA_API_KEY)
         .then(function (response) {
             console.log(response);
@@ -25,7 +45,7 @@ function Home() {
                     {data.length === 0 
                         ? "Loading data..."
                         : data.map((el) => (
-                            <div key={el} className="card card-sm mb-3 mx-auto">
+                            <div key={el.title} className="card card-sm mb-3 mx-auto">
                                 <div className="card-body">
                                     <div className="mb-3">
                                         {
@@ -36,6 +56,11 @@ function Home() {
                                     </div>
                                     <p>{el.title}</p>
                                     <p className="text-muted">{el.date}</p>
+                                    {
+                                        likedImages[md5(el.title)] !== undefined 
+                                        ? <button id={md5(el.title)} className="btn-like" onClick={(e) => like(e)}><i className="fas fa-heart"></i></button>
+                                        : <button id={md5(el.title)} className="btn-like" onClick={(e) => like(e)}><i className="far fa-heart"></i></button>
+                                    }
                                 </div>
                             </div>
                     ))}
